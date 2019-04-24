fbe780814dcfa3117a4bd4da58847564ab99c587
vsharmanov <v.v.sharmanov@gmail.com>
2019-04-24 10:19:00 +0700

    Cleaned after conversion

--- YAPM/Wmi/Objects/Thread.cs
+++ YAPM/Wmi/Objects/Thread.cs
@@ -8,9 +8,6 @@ namespace Wmi.Objects
 {
     public class Thread
     {
-
-
-
         // ========================================
         // Private constants
         // ========================================
@@ -26,9 +23,10 @@ namespace Wmi.Objects
         // ========================================
 
         // Enumerate threads
-        public static bool EnumerateThreadByIds(int pid, System.Management.ManagementObjectSearcher objSearcher, ref Dictionary<string, threadInfos> _dico, ref string errMsg)
+        public static bool EnumerateThreadByIds(int pid, ManagementObjectSearcher objSearcher,
+            ref Dictionary<string, threadInfos> dico, ref string errMsg)
         {
-            ManagementObjectCollection res = null;
+            ManagementObjectCollection res;
             try
             {
                 res = objSearcher.Get();
@@ -39,49 +37,66 @@ namespace Wmi.Objects
                 return false;
             }
 
-            foreach (System.Management.ManagementObject refThread in res)
+            foreach (var o in res)
             {
-                int wmiId = System.Convert.ToInt32(refThread.GetPropertyValue(Native.Api.Enums.WmiInfoThread.ProcessHandle.ToString()));
+                var refThread = (ManagementObject) o;
+                var wmiId = Convert.ToInt32(
+                    refThread.GetPropertyValue(Enums.WmiInfoThread.ProcessHandle.ToString()));
 
                 // If we have to get threads for this process...
-                if (pid == wmiId)
+                if (pid != wmiId) continue;
+                var obj =
+                    new NativeStructs.SystemThreadInformation();
                 {
-                    Native.Api.NativeStructs.SystemThreadInformation obj = new Native.Api.NativeStructs.SystemThreadInformation();
+                    obj.BasePriority = Convert.ToInt32(
+                        refThread.GetPropertyValue(Enums.WmiInfoThread.PriorityBase.ToString()));
+                    obj.CreateTime = 0;
+                    obj.ClientId = new NativeStructs.ClientId(wmiId,
+                        Convert.ToInt32(
+                            refThread.GetPropertyValue(Enums.WmiInfoThread.Handle.ToString())));
+                    obj.KernelTime = 10000 * Convert.ToInt32(
+                                         refThread.GetPropertyValue(Enums.WmiInfoThread
+                                             .KernelModeTime.ToString()));
+                    obj.Priority =
+                        Convert.ToInt32(
+                            refThread.GetPropertyValue(Enums.WmiInfoThread.Priority.ToString()));
+                    try
                     {
-                        var withBlock = obj;
-                        withBlock.BasePriority = System.Convert.ToInt32(refThread.GetPropertyValue(Native.Api.Enums.WmiInfoThread.PriorityBase.ToString()));
-                        withBlock.CreateTime = 0;
-                        withBlock.ClientId = new Native.Api.NativeStructs.ClientId(wmiId, System.Convert.ToInt32(refThread.GetPropertyValue(Native.Api.Enums.WmiInfoThread.Handle.ToString())));
-                        withBlock.KernelTime = 10000 * System.Convert.ToInt32(refThread.GetPropertyValue(Native.Api.Enums.WmiInfoThread.KernelModeTime.ToString()));
-                        withBlock.Priority = System.Convert.ToInt32(refThread.GetPropertyValue(Native.Api.Enums.WmiInfoThread.Priority.ToString()));
-                        try
-                        {
-                            withBlock.StartAddress = (IntPtr)refThread.GetPropertyValue(Native.Api.Enums.WmiInfoThread.StartAddress.ToString());
-                        }
-                        catch (Exception ex0)
-                        {
-                            withBlock.StartAddress = NativeConstants.InvalidHandleValue;
-                        }
-                        withBlock.State = (ThreadState)refThread.GetPropertyValue(Native.Api.Enums.WmiInfoThread.ThreadState.ToString());
-                        withBlock.UserTime = 10000 * System.Convert.ToInt32(refThread.GetPropertyValue(Native.Api.Enums.WmiInfoThread.UserModeTime.ToString()));
-                        withBlock.WaitReason = (Native.Api.NativeEnums.KwaitReason)System.Convert.ToInt32(refThread.GetPropertyValue(Native.Api.Enums.WmiInfoThread.ThreadWaitReason.ToString()));
-                        try
-                        {
-                            withBlock.WaitTime = 10000 * System.Convert.ToInt32(refThread.GetPropertyValue(Native.Api.Enums.WmiInfoThread.ElapsedTime.ToString()));
-                        }
-                        catch (Exception ex1)
-                        {
-                        }
+                        obj.StartAddress =
+                            (IntPtr) refThread.GetPropertyValue(
+                                Enums.WmiInfoThread.StartAddress.ToString());
                     }
-                    threadInfos _procInfos = new threadInfos(ref obj);
-                    string _key = obj.ClientId.UniqueThread.ToString() + "-" + obj.ClientId.UniqueProcess.ToString();
-                    if (_dico.ContainsKey(_key) == false)
-                        _dico.Add(_key, _procInfos);
+                    catch (Exception)
+                    {
+                        obj.StartAddress = NativeConstants.InvalidHandleValue;
+                    }
+
+                    obj.State =
+                        (ThreadState) refThread.GetPropertyValue(
+                            Enums.WmiInfoThread.ThreadState.ToString());
+                    obj.UserTime = 10000 * Convert.ToInt32(
+                                       refThread.GetPropertyValue(Enums.WmiInfoThread.UserModeTime
+                                           .ToString()));
+                    obj.WaitReason = (NativeEnums.KwaitReason) Convert.ToInt32(
+                        refThread.GetPropertyValue(Enums.WmiInfoThread.ThreadWaitReason.ToString()));
+                    try
+                    {
+                        obj.WaitTime = 10000 * Convert.ToInt32(
+                                           refThread.GetPropertyValue(Enums.WmiInfoThread
+                                               .ElapsedTime.ToString()));
+                    }
+                    catch
+                    {
+                        // ignored
+                    }
                 }
+                var procInfos = new threadInfos(ref obj);
+                var key = obj.ClientId.UniqueThread + "-" + obj.ClientId.UniqueProcess;
+                if (dico.ContainsKey(key) == false)
+                    dico.Add(key, procInfos);
             }
 
             return true;
         }
     }
 }
-
